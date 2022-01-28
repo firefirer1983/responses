54dda033b94cc438de0d990c9c6abfe54fa5aeb4
firefirer1983 <fyman.zhang@gmail.com>
2022-01-28 15:49:55 +0800

    增添版本文件 __version__.py
    setup.py依赖于 __version__.py

---
+++ responses/__version__.py
@@ -0,0 +1,17 @@
+#  ____  _____ ____  ____  ____  _      ____  _____
+# /  __\/  __// ___\/  __\/  _ \/ \  /|/ ___\/  __/
+# |  \/||  \  |    \|  \/|| / \|| |\ |||    \|  \
+# |    /|  /_ \___ ||  __/| \_/|| | \||\___ ||  /_
+# \_/\_\\____\\____/\_/   \____/\_/  \|\____/\____\
+#
+
+__title__ = 'responses'
+__description__ = 'Learn From Great Requests.'
+__url__ = 'https://github.com/firefirer1983/responses.git'
+__version__ = '0.0.1'
+__build__ = 0x000001
+__author__ = 'Zhang XuYi'
+__author_email__ = 'fyman.zhang@icloud.com'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2022 Zhang XuYi'
+__cake__ = u'\u2728 \U0001f370 \u2728'
